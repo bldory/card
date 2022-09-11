0907ef716bced30e8df934a5c091ff8ad9bc499e
bldory <62130422+bldory@users.noreply.github.com>
2022-09-11 21:38:24 +0900

    확률 비유기 없데이트

--- resources/pages/persent/main.js
+++ resources/pages/persent/main.js
@@ -1,8 +1,17 @@
 var percent = [
-  { name: "윷놀이에서 모가 나올 확률", per: 2.56, number: "0" },
-  { name: "윷놀이에서 윷이 나올 확률", per: 12.96, number: "1" },
-  { name: "윷놀이에서 도가 나올 확률", per: 15.36, number: "2" },
-  { name: "윷놀이에서 개, 걸이 나올 확률", per: 34.56, number: "3" },
+  { name: "윷놀이에서 모가 나올 확률", per: 25600, number: "0" },
+  { name: "윷놀이에서 윷이 나올 확률", per: 129600, number: "1" },
+  { name: "윷놀이에서 도가 나올 확률", per: 153600, number: "2" },
+  { name: "윷놀이에서 개, 걸이 나올 확률", per: 345600, number: "3" },
+  { name: "모쏠이 여친생길 확률", per: 0, number: "4" },
+  { name: "쿠킹덤에서 레전나올 확률", per: 540, number: "5" },
+  { name: "길가다가 벼락맞을 확률", per: 35, number: "6" },
+  { name: "로또 당첨 확률", per: 1.25, number: "7" },
+  {
+    name: "7907 차량이 실수로 차를 박았는데 사고당한 차량도 7907이고, 출동한 경찰차도 7907일 확률",
+    per: 0.1,
+    number: "8",
+  },
 ];
 
 var inp = document.getElementById("per");
@@ -22,7 +31,7 @@ function closest(num, arr) {
 }
 
 inp.addEventListener("input", function () {
-  var perc = closest(inp.value, percent);
+  var perc = closest(inp.value * 10000, percent);
   console.log();
-  res.innerText = perc.per + "% : " + perc.name;
+  res.innerText = perc.per / 10000 + "% : " + perc.name;
 });
